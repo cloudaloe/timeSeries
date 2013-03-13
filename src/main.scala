175d3d82b9c4d0225452fd4f2b92c24443f8cd48
matan <matan@cloudaloe.org>
2013-03-13 10:20:42 +0200

    init

---
+++ src/main.scala
@@ -0,0 +1,12 @@
+object main {
+	def main(args: Array[String]) {
+	  
+	    println("Main starting")
+ 
+	    
+		//curl examples at http://www.yilmazhuseyin.com/blog/dev/curl-tutorial-examples-usage/
+		//val httpGet1 = Seq("curl", "localhost:8081/aaaaa") lines_! ProcessLogger(line => ()) // execute and suppress stderr
+		//val httpGet2 = Seq("curl", "localhost:8082/aaaaa") lines_! ProcessLogger(line => ()) // execute and suppress stderr
+	     
+	}
+}
